78f136354551a1a37647ebe0bc4826ea13de4558
Navid-Meng <navidmeng3107@gmail.com>
2025-08-21 16:10:21 +0700

    feat: change the signature line

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -136,7 +136,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Heart.`,
     },
     prism: {
       theme: prismThemes.github,
